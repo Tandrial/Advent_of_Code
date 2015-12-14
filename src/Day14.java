77e045f42b7ef99ed1c46adb40a317defef0e633
Michael Krane <michael.krane@gmx.de>
2015-12-14 16:04:30 +0100

    Day 14 - STREAAAAAAAAAAAAM powa

--- src/Day14.java
+++ src/Day14.java
@@ -9,33 +9,20 @@ public class Day14 {
 	static long maxDistance = 0;
 	static long maxPoints = 0;
 
-	public static long partOne(List<String> list, int dur) {
-		simulate(list, dur);
-		return maxDistance;
-	}
-
-	public static long partTwo(List<String> list, int dur) {
-		simulate(list, dur);
-		return maxPoints;
-	}
-
 	public static void simulate(List<String> list, int dur) {
 		List<Reindeer> reindeers = parseReindeer(list);
 		maxDistance = 0;
 		maxPoints = 0;
-
-		Reindeer.maxDistance = 0;
 		for (int i = 0; i < dur; i++) {
-			for (Reindeer r : reindeers) {
-				r.tick();
-			}
-			for (Reindeer r : reindeers) {
-				if (r.distance == Reindeer.maxDistance)
-					r.points++;
-				maxDistance = Math.max(maxDistance, r.distance);
-				maxPoints = Math.max(maxPoints, r.points);
-			}
+			reindeers.forEach(Reindeer::tick);
+			reindeers.forEach(Reindeer::addPoints);
 		}
+		reindeers.forEach(Day14::updateStats);
+	}
+
+	private static void updateStats(Reindeer r) {
+		maxDistance = Math.max(maxDistance, r.distance);
+		maxPoints = Math.max(maxPoints, r.points);
 	}
 
 	private static List<Reindeer> parseReindeer(List<String> list) {
@@ -45,8 +32,8 @@ public class Day14 {
 				String[] line = t.split(" ");
 				Reindeer r = new Reindeer();
 				r.fly_speed = Integer.parseInt(line[3]);
-				r.fly_durr = Integer.parseInt(line[6]);
-				r.rest_durr = Integer.parseInt(line[13]);
+				r.fly_dur = Integer.parseInt(line[6]);
+				r.rest_dur = Integer.parseInt(line[13]);
 				return r;
 			}
 		}).collect(Collectors.toList());
@@ -54,31 +41,36 @@ public class Day14 {
 
 	public static void main(String[] args) throws IOException {
 		List<String> s = Files.readAllLines(Paths.get("./input/Day14_input.txt"));
-		System.out.println("Part One = " + partOne(s, 2503));
-		System.out.println("Part Two = " + partTwo(s, 2503));
+		simulate(s, 2503);
+		System.out.println("Part One = " + Day14.maxDistance);
+		System.out.println("Part Two = " + Day14.maxPoints);
 	}
 }
 
 class Reindeer {
+	public static int maxDistance = 0;
 
-	public static int maxDistance;
-
-	int fly_speed, fly_durr, rest_durr;
+	int fly_speed, fly_dur, rest_dur;
 	int points, distance, counter;
 	boolean flying = true;
 
 	public void tick() {
 		counter++;
 		if (flying) {
-			if (counter == fly_durr) {
+			if (counter == fly_dur) {
 				flying = false;
 				counter = 0;
 			}
 			distance += fly_speed;
 			Reindeer.maxDistance = Math.max(maxDistance, distance);
-		} else if (counter == rest_durr) {
+		} else if (counter == rest_dur) {
 			flying = true;
 			counter = 0;
 		}
+	}
+
+	public void addPoints() {
+		if (distance == Reindeer.maxDistance)
+			points++;
 	}
 }
