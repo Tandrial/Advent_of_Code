05eca9de8623078c6d1a73f13318e786dc8162fb
Michael Krane <Michael.Krane@gmx.de>
2016-12-02 07:23:49 +0100

    AoC 2016 - Day 02 + Day 01 improvments

--- src/aoc2016/Day01.java
+++ src/aoc2016/Day01.java
@@ -1,54 +1,37 @@
 package aoc2016;
 
+import java.awt.Point;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
 
 public class Day01 {
+  private int[][]    moves     = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+  private Point[]    sol       = { new Point(), null };
 
-  private int[][]                    moves     = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-  private int[][]                    sol       = { { 0, 0 }, { 0, 0 } };
+  private Set<Point> visited   = new HashSet<>();
+  private int        direction = 0;
 
-  private boolean                    found     = false;
-  private Map<Integer, Set<Integer>> visited   = new HashMap<>();
-  private int                        direction = 0;
-
-  public Day01(String string) {
+  public void solve(String string) {
     for (String s : string.split(", ")) {
       direction = Math.floorMod(direction + (s.charAt(0) == 'L' ? -1 : 1), 4);
 
       int amount = Integer.parseInt(s.substring(1));
-
       while (amount-- > 0) {
-        for (int pos = 0; pos < moves[0].length; pos++)
-          sol[0][pos] += moves[direction][pos];
+        sol[0].translate(moves[direction][0], moves[direction][1]);
 
-        if (!found) {
-          if (!visited.containsKey(sol[0][0]))
-            visited.put(sol[0][0], new HashSet<>());
-          if (visited.get(sol[0][0]).contains(sol[0][1])) {
-            found = true;
-            sol[1] = sol[0].clone();
-          } else
-            visited.get(sol[0][0]).add(sol[0][1]);
-        }
+        if (sol[1] == null && visited.contains(sol[0]))
+          sol[1] = new Point(sol[0]);
+        else
+          visited.add(sol[0]);
       }
     }
-  }
-
-  private int partOne() {
-    return Math.abs(sol[0][0]) + Math.abs(sol[0][1]);
-  }
-
-  private int partTwo() {
-    return Math.abs(sol[1][0]) + Math.abs(sol[1][1]);
+    System.out.println(String.format("Part one = %d\nPart two = %d", Math.abs(sol[0].x) + Math.abs(sol[0].y),
+        Math.abs(sol[1].x) + Math.abs(sol[1].y)));
   }
 
   public static void main(String[] args) throws IOException {
     String s = new String(Files.readAllBytes(Paths.get("./input/2016/Day01_input.txt")));
-
-    Day01 day1 = new Day01(s);
-    System.out.println("Part one = " + day1.partOne());
-    System.out.println("Part two = " + day1.partTwo());
+    new Day01().solve(s);
   }
 }
---
+++ src/aoc2016/Day02.java
@@ -0,0 +1,36 @@
+package aoc2016;
+
+import java.awt.Point;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+public class Day02 {
+  public static String solve(Point pos, char[][] keypad, List<String> xs) {
+    StringBuilder password = new StringBuilder();
+    for (String s : xs) {
+      for (char c : s.toCharArray()) {
+        Point old = new Point(pos);
+        if (c == 'U') pos.x = Math.max(0, pos.x - 1);
+        else if (c == 'L') pos.y = Math.max(0, pos.y - 1);
+        else if (c == 'D') pos.x = Math.min(keypad.length - 1, pos.x + 1);
+        else if (c == 'R') pos.y = Math.min(keypad[0].length - 1, pos.y + 1);
+        
+        if (keypad[pos.x][pos.y] == 0)
+          pos = old;
+      }
+      password.append(keypad[pos.x][pos.y]);
+    }
+    return password.toString();
+  }
+
+  public static void main(String[] args) throws IOException {
+    List<String> s = Files.readAllLines(Paths.get("./input/2016/Day02_input.txt"));
+    char[][] keypad1 = { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+    char[][] keypad2 = { { 0, 0, '1', 0, 0 }, { 0, '2', '3', '4', 0 }, { '5', '6', '7', '8', '9' },
+        { 0, 'A', 'B', 'C', 0 }, { 0, 0, 'D', 0, 0 } };
+
+    System.out.println("Part One = " + solve(new Point(1, 1), keypad1, s));
+    System.out.println("Part Two = " + solve(new Point(2, 0), keypad2, s));
+  }
+}
