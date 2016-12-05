93068b5b725b9d4656a7142e5342977cbee163db
Michael Krane <Michael.Krane@gmx.de>
2016-12-05 07:50:49 +0100

    AoC 2016 - Day05

---
+++ src/aoc2016/Day05.java
@@ -0,0 +1,35 @@
+package aoc2016;
+
+import java.security.*;
+
+public class Day05 {
+  public static String solve(String s, boolean partTwo) throws NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("md5");
+    char[] sb = new char[8];
+    String result = "";
+    long cnt = 0;
+    int charsFound = 0;
+    while (charsFound < 8) {
+      do {
+        byte[] inputBytes = (s + cnt++).getBytes();
+        byte[] hashBytes = digest.digest(inputBytes);
+        result = javax.xml.bind.DatatypeConverter.printHexBinary(hashBytes);
+      } while (!result.startsWith("00000"));
+      if (!partTwo) {
+        sb[charsFound++] = result.charAt(5);
+      } else {
+        if (result.charAt(5) < '8' && sb[result.charAt(5) - '0'] == 0) {
+          sb[result.charAt(5) - '0'] = result.charAt(6);
+          charsFound++;
+        }
+      }
+    }
+    return String.valueOf(sb);
+  }
+
+  public static void main(String[] args) throws NoSuchAlgorithmException {
+    String s = "ugkcyxxp";
+    System.out.println("Part One = " + solve(s, false));
+    System.out.println("Part Two = " + solve(s, true));
+  }
+}
