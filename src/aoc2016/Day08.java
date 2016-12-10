dc10bd401d9e3ae969a93c6b899a6bccba28d3fb
Michael Krane <Michael.Krane@gmx.de>
2016-12-10 14:10:58 +0100

    AoC 2016 - Day08 - optimazation

--- src/aoc2016/Day08.java
+++ src/aoc2016/Day08.java
@@ -1,75 +1,54 @@
 package aoc2016;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.file.*;
+import java.util.*;
+import java.util.function.UnaryOperator;
+import java.util.stream.*;
 
 public class Day08 {
 
-  public static int partOne(List<String> input) {
-    boolean[][] arr = new boolean[6][50];
+  public static void solve(List<String> input) {
+    int[][] lcd = new int[6][50];
     for (String s : input) {
       if (s.startsWith("rect")) {
         int a = Integer.valueOf(s.split(" ")[1].split("x")[0]);
         int b = Integer.valueOf(s.split(" ")[1].split("x")[1]);
-        arr = rect(a, b, arr);
+        lcd = rect(a, b, lcd);
       } else if (s.startsWith("rotate")) {
         int x = Integer.valueOf(s.split("=")[1].split(" ")[0]);
         int by = Integer.valueOf(s.split("by ")[1]);
-        arr = s.contains("row") ? rotRow(x, by, arr) : rotCol(x, by, arr);
+        lcd = s.contains("row") ? rotRow(x, by, lcd) : transpose().apply(rotRow(x, by, transpose().apply(lcd)));
       }
     }
-    int cnt = 0;
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr[0].length; j++) {
-        if (arr[i][j]) {
-          cnt++;
-          System.out.print('#');
-        } else
-          System.out.print(' ');
-      }
-      System.out.println();
-    }
-    return cnt;
+    System.out.println(String.format("Part One = %d\nPart Two = ", Arrays.stream(lcd).map(xs -> Arrays.stream(xs).sum()).reduce(0, Integer::sum)));
+    Arrays.stream(lcd).map(xs -> Arrays.stream(xs).mapToObj(x -> String.valueOf(x == 1 ? '#' : ' ')).collect(Collectors.joining())).forEachOrdered(System.out::println);
   }
 
-  public static boolean[][] rect(int a, int b, boolean[][] arr) {
-    boolean[][] result = new boolean[arr.length][];
+  public static UnaryOperator<int[][]> transpose() {
+    return m -> { return IntStream.range(0, m[0].length).mapToObj(r -> IntStream.range(0, m.length).map(c -> m[c][r]).toArray()).toArray(int[][]::new); };
+  }
+  
+  public static int[][] rect(int a, int b, int[][] arr) {
+    int[][] result = new int[arr.length][];
     for (int i = 0; i < arr.length; i++)
       result[i] = Arrays.copyOf(arr[i], arr[i].length);
     for (int i = 0; i < b; i++)
-      for (int j = 0; j < a; j++)
-        result[i][j] = true;
+      Arrays.fill(result[i], 0, a, 1);
     return result;
   }
 
-  public static boolean[][] rotRow(int y, int by, boolean[][] arr) {
-    boolean[][] result = new boolean[arr.length][];
+  public static int[][] rotRow(int y, int by, int[][] arr) {
+    int[][] result = new int[arr.length][];
     for (int i = 0; i < arr.length; i++)
       result[i] = Arrays.copyOf(arr[i], arr[i].length);
-    for (int i = result[y].length - 1; i >= by; i--)
-      result[y][i] = arr[y][i - by];
-    for (int i = 0; i < by; i++)
-      result[y][i] = arr[y][arr[0].length - by + i];
-    return result;
-  }
-
-  public static boolean[][] rotCol(int x, int by, boolean[][] arr) {
-    return transpose(rotRow(x, by, transpose(arr)));
-  }
-
-  public static boolean[][] transpose(boolean[][] arr) {
-    boolean[][] result = new boolean[arr[0].length][arr.length];
-    for (int i = 0; i < result.length; i++)
-      for (int j = 0; j < result[0].length; j++)
-        result[i][j] = arr[j][i];
+    for (int i = 0; i < result[y].length; i++)
+      result[y][i] = arr[y][Math.floorMod(i - by, result[y].length)];
     return result;
   }
 
   public static void main(String[] args) throws IOException {
     List<String> lines = Files.readAllLines(Paths.get("./input/2016/Day08_input.txt"));
-    System.out.println("Part One = " + partOne(lines));
+    solve(lines);
   }
 }
