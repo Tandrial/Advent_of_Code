c31ed08197d7a5a3bf21a57141f7e0d4e9e87760
Michael Krane <Michael.Krane@gmx.de>
2016-12-10 22:06:52 +0100

    AoC 2016 - Day10 optimazation

--- src/aoc2016/Day10.java
+++ src/aoc2016/Day10.java
@@ -6,50 +6,43 @@ import java.util.*;
 import java.util.regex.*;
 import java.util.stream.IntStream;
 
-class Bot {
-  static String partOne;
-  static int[]  cmp    = { -1, -1 };
-
-  String        id     = "";
-  int[]         values = { -1, -1 };
-  String[]      passTo = { "", "" };
-
-  public Bot(String id, String idLow, String idHigh) {
-    this.id = id;
-    passTo = new String[] { idHigh, idLow };
-  }
-
-  public Collection<String> accept(int value) {
-    if (value > values[0])
-      values = new int[] { value, values[0] };
-    else
-      values[1] = value;
-
-    Collection<String> result = new ArrayList<>();
-    if (values[0] != -1 && values[1] != -1) {
-      if (values[0] == Bot.cmp[0] && values[1] == Bot.cmp[1])
-        Bot.partOne = id;
-      for (int i = 0; i < values.length; i++)
-        result.add(String.format("value %d goes to %s", values[i], passTo[i]));
-      values = new int[] { -1, -1 };
-    }
-    return result;
-  }
-}
-
 public class Day10 {
+  private static class Bot {
+    static String          partOne;
+    String                 id     = "";
+    PriorityQueue<Integer> values = new PriorityQueue<>(2, (o1, o2) -> o2.compareTo(o1));
+    String[]               passTo = { "", "" };
+
+    public Bot(String id, String idLow, String idHigh) {
+      this.id = id;
+      this.passTo = new String[] { idHigh, idLow };
+    }
+
+    public Collection<String> accept(int value, int[] cmp) {
+      values.add(value);
+      Collection<String> result = new ArrayList<>();
+      if (values.size() == 2) {
+        int[] val = { values.poll(), values.poll() };
+        if (val[0] == cmp[0] && val[1] == cmp[1])
+          Bot.partOne = id;
+        for (int i = 0; i < val.length; i++)
+          result.add(String.format("value %d goes to %s", val[i], passTo[i]));
+      }
+      return result;
+    }
+  }
+
   static Map<String, Bot> bots = new HashMap<>();
 
   public static String partOne(List<String> input, int[] cmp) {
-    Bot.cmp = cmp;
     Queue<String> moves = new ArrayDeque<>(input);
     while (!moves.isEmpty()) {
       String move = moves.poll();
-      Matcher valueAdd = Pattern.compile("value (\\d+) goes to (output \\d+|bot \\d+)").matcher(move);
-      Matcher valueMove = Pattern.compile("(bot \\d+) gives low to (output \\d+|bot \\d+) and high to (output \\d+|bot \\d+)").matcher(move);
-      
+      Matcher valueAdd = Pattern.compile("value (\\d+) goes to (\\S+ \\d+)").matcher(move);
+      Matcher valueMove = Pattern.compile("(bot \\d+) gives low to (\\S+ \\d+) and high to (\\S+ \\d+)").matcher(move);
+
       if (valueAdd.matches() && (bots.containsKey(valueAdd.group(2)) || valueAdd.group(2).startsWith("output")))
-        moves.addAll(bots.computeIfAbsent(valueAdd.group(2), x -> new Bot(x, "", "")).accept(Integer.valueOf(valueAdd.group(1))));
+        moves.addAll(bots.computeIfAbsent(valueAdd.group(2), x -> new Bot(x, "", "")).accept(Integer.valueOf(valueAdd.group(1)), cmp));
       else if (valueMove.matches())
         bots.put(valueMove.group(1), new Bot(valueMove.group(1), valueMove.group(2), valueMove.group(3)));
       else
@@ -59,7 +52,7 @@ public class Day10 {
   }
 
   public static int partTwo(int max) {
-    return IntStream.range(0, max + 1).map(x -> bots.get("output " + String.valueOf(x)).values[0]).reduce(1, Math::multiplyExact);
+    return IntStream.range(0, max + 1).map(x -> bots.get("output " + String.valueOf(x)).values.peek()).reduce(1, Math::multiplyExact);
   }
 
   public static void main(String[] args) throws IOException {
