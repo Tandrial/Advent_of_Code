5a763fb3be0dc1f91050ebc90e56e320988aba35
Michael Krane <michael.krane@gmx.de>
2016-12-18 19:15:15 +0100

    AoC 2016 - Day 17&18

---
+++ src/aoc2016/Day18.java
@@ -0,0 +1,20 @@
+package aoc2016;
+
+public class Day18 {
+    private static long solve(String input, int roundCount) {
+        long cnt = 0;
+        for (int round = 1; round <= roundCount; round++) {
+            cnt += input.chars().map(x -> x == '.' ? 1 : 0).sum();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.length(); i++)
+                sb.append((i == 0 ? '.' : input.charAt(i - 1)) != (i == input.length() -1 ? '.' : input.charAt(i + 1)) ? '^' : '.');
+            input = sb.toString();
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Part One = " + solve("^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^", 40));
+        System.out.println("Part Two = " + solve("^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^", 400000));
+    }
+}
