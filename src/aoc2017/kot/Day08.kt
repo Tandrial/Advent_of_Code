229b7cb9e81a44f505b4a197307b714362377a31
Michael Krane <Michael.Krane@gmx.de>
2017-12-08 06:46:39 +0100

    AoC 2017 - Day 8

---
+++ src/aoc2017/kot/Day08.kt
@@ -0,0 +1,45 @@
+package aoc2017.kot
+
+import java.io.File
+import java.util.regex.Pattern
+
+object Day08 {
+
+  fun solve(input: List<String>): Pair<Int, Int> {
+    val memory = mutableMapOf<String, Int>()
+    val regex = Pattern.compile("(\\w+) (inc|dec) (-?\\d+) if (\\w+) (>|<|==|>=|<=|!=) (-?\\d+)")
+    var maxCurr = -1
+    for (line in input) {
+      val m = regex.matcher(line)
+      if (m.find()) {
+        val reg = m.group(1).toString()
+        var mod = m.group(3).toInt()
+        if (m.group(2).toString() == "dec") mod *= -1
+
+        val checkReg = m.group(4).toString()
+        val value = m.group(6).toInt()
+
+        val cmp: (Int, Int) -> Boolean = when (m.group(5)) {
+          ">" -> { r, v -> r > v }
+          "<" -> { r, v -> r < v }
+          ">=" -> { r, v -> r >= v }
+          "<=" -> { r, v -> r <= v }
+          "==" -> { r, v -> r == v }
+          "!=" -> { r, v -> r != v }
+          else -> { _, _ -> false }
+        }
+
+        if (cmp(memory.getOrPut(checkReg, { 0 }), value)) memory[reg] = memory.getOrPut(reg, { 0 }) + mod
+        maxCurr = Math.max(memory.getOrPut(reg, { 0 }), maxCurr)
+      }
+    }
+    return Pair(memory.values.max()!!, maxCurr)
+  }
+}
+
+fun main(args: Array<String>) {
+  val input = File("./input/2017/Day08_input.txt").readLines()
+  val result = Day08.solve(input)
+  println("Part One = ${result.first}")
+  println("Part Two = ${result.second}")
+}
