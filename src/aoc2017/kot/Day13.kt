f8d14e7383ef0fa0c047a0e025b915fccc6212a6
Michael Krane <Michael.Krane@gmx.de>
2017-12-13 09:09:33 +0100

    AoC 2017 - Day 13 - bruteforce done for Part2

--- src/aoc2017/kot/Day13.kt
+++ src/aoc2017/kot/Day13.kt
@@ -4,8 +4,7 @@ import java.io.File
 
 object Day13 {
 
-  data class Layer(val depth: Int, val range: Int, var currPos: Int = 0) {
-    var dir = -1
+  data class Layer(val depth: Int, val range: Int, var currPos: Int = 0, var dir: Int = -1) {
     fun move() {
       if (currPos == range - 1) dir = -dir
       else if (currPos == 0) dir = -dir
@@ -13,55 +12,48 @@ object Day13 {
     }
 
     fun move(wait: Int) {
-      if (wait == 0) {
-        dir = -1
-        currPos = 0
-      } else {
-        val offset = wait % (2 * range - 1)
-        if (offset > range) {
-          dir = 1
-          currPos = offset - range
-        } else if (offset == range) {
-          dir = -1
-          currPos = range
-        } else if (offset == 0) {
-          dir = -1
-          currPos = 0
-        } else {
-          dir = -1
-          currPos = offset
-        }
-      }
+//      val offset = wait % (2 * range - 1)
+//      repeat(offset) { move() }
+//      if (offset >= range) {
+      repeat(wait) { move() }
+//        dir = -1
+//        currPos = offset - range
+//      } else if (offset == 0) {
+//        dir = -1
+//        currPos = 0
+//      }else {
+//        dir = 1
+//        currPos = offset
+//      }
     }
 
     fun sev(): Int = depth * range
 
-    fun reset() {
-      dir = -1
-      currPos = 0
-    }
   }
 
-  fun partOne(input: List<Layer>, wait: Int): Int {
+  fun partOne(input: List<String>, wait: Int): Int {
+    val layers = parse(input)
     var packetPos = -1
     var sum = 0
 
-    input.forEach { it.move(wait) }
+    layers.forEach { it.move(wait) }
 
     val bal = 0
     while (true) {
       packetPos++
-      if (packetPos != 0 || wait > 0) sum += input.filter { it.depth == packetPos && it.currPos == 0 }.sumBy { maxOf(it.sev(), 1) }
-      input.forEach { it.move() }
-      if (packetPos == input.last().depth) break
+      if (packetPos != 0 || wait > 0) sum += layers.filter { it.depth == packetPos && it.currPos == 0 }.sumBy { maxOf(it.sev(), 1) }
+      layers.forEach { it.move() }
+      if (packetPos == layers.last().depth) break
     }
 
     return sum
   }
 
-  fun partTwo(input: List<Layer>): Int {
+  fun partTwo(input: List<String>): Int {
     var wait = 0
     while (true) {
+      if (wait == 3)
+        println()
       if (partOne(input, wait) == 0) break
       wait++
       if (wait % 1000 == 0) println(wait)
@@ -69,16 +61,12 @@ object Day13 {
     return wait
   }
 
-//  fun parse(input: List<String>): List<?> {
-//    val result = listOf<>()
-//
-//    return result
-//  }
+  fun parse(input: List<String>): List<Layer> = input.map { val line = it.split(": "); Layer(line[0].toInt(), line[1].toInt()) }
 
 }
 
 fun main(args: Array<String>) {
-  val input = File("./input/2017/Day13_input.txt").readLines().map { val line = it.split(": "); Day13.Layer(line[0].toInt(), line[1].toInt()) }
+  val input = File("./input/2017/Day13_input.txt").readLines()
 
   println("Part One = ${Day13.partOne(input, 0)}")
   println("Part Two = ${Day13.partTwo(input)}")
