607e949c0280ab484b3282e49740ea70d731345a
Michael Krane <Michael.Krane@gmx.de>
2017-12-15 14:00:40 +0100

    AoC 2017 - Day 15

---
+++ src/aoc2017/kot/Day15.kt
@@ -0,0 +1,42 @@
+package aoc2017.kot
+
+import getWords
+import java.io.File
+import kotlin.coroutines.experimental.buildSequence
+
+object Day15 {
+
+  fun partOne(seedA: Long, seedB: Long): Int {
+    val genA = gen(seedA, 16807)
+    val genB = gen(seedB, 48271)
+    return getCount(genA, genB, 40_000_000)
+  }
+
+  fun partTwo(seedA: Long, seedB: Long): Int {
+    val genA = gen(seedA, 16807, 4)
+    val genB = gen(seedB, 48271, 8)
+    return getCount(genA, genB, 5_000_000)
+  }
+
+  private fun gen(seed: Long, mult: Int, filter: Long = 1): Sequence<Long> = buildSequence {
+    var state = seed
+    while (true) {
+      do {
+        state = (state * mult) % Int.MAX_VALUE
+      } while (state and (filter - 1) != 0L)
+      yield(state)
+    }
+  }
+
+  private fun getCount(seqA: Sequence<Long>, seqB: Sequence<Long>, length: Int): Int {
+    val values = seqA.zip(seqB).take(length)
+    return values.count { (a, b) -> a and 0xFFFF == b and 0xFFFF }
+  }
+}
+
+fun main(args: Array<String>) {
+  val input = File("./input/2017/Day15_input.txt").readLines()
+  val seeds = input.map { it.getWords()[4].toLong() }
+  println("Part One = ${Day15.partOne(seeds[0], seeds[1])}")
+  println("Part Two = ${Day15.partTwo(seeds[0], seeds[1])}")
+}
